29b41f11ec97f9e8f9c2efb0697334a130b14e7f
jiduqiu <2556800040@qq.com>
2025-02-15 18:48:10 +0800

    Initial commit

---
+++ src/main/java/com/erpandheihu/ErPandHeihuApplication.java
@@ -0,0 +1,13 @@
+package com.erpandheihu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErPandHeihuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErPandHeihuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/erpandheihu/ErPandHeihuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erpandheihu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErPandHeihuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
