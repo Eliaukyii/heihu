ee0f903214afa99e6e416460e6d45234bc66983b
name <email>
2025-02-19 22:23:19 +0800

    2.19.2222

---
+++ business/src/main/java/com/example/business/controller/OtherDispatchCreateController.java
@@ -0,0 +1,38 @@
+package com.example.business.controller;
+
+import com.example.business.service.processor.ProcessorService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+    @RestController
+    @RequestMapping("/OtherDispatchCreateApi")
+    @Slf4j
+public class OtherDispatchCreateController {
+
+    @Autowired
+    private ProcessorService processorService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @PostMapping("/OtherDispatchCreate")
+    public ResponseEntity<String>  OtherDispatchCreate(@RequestBody Map<String,Object> message){
+        try {
+            String jsonMessage = objectMapper.writeValueAsString(message);
+            return new ResponseEntity<>(jsonMessage, HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>("Error converting message to JSON",HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+}
---
+++ business/src/main/java/com/example/business/domain/OtherDispatchCreateParams.java
@@ -0,0 +1,34 @@
+package com.example.business.domain;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+public class OtherDispatchCreateParams {
+    private static String Url;
+    private static String Uri;
+    private static String appKey;
+    private static String appSecret;
+
+    @Value("${params.url}")
+    public void setUrl(String url){
+        ApiParams.url = url;
+    }
+
+    @Value("${OtherDispatchCreateParams.uri}")
+    public void setOtherDispatchCreateUri(String uri) {
+        OtherDispatchCreateParams.Uri = Uri;
+    }
+
+    @Value("${params.appKey}")
+    public void setAppKey(String appKey) {
+        ApiParams.appKey = appKey;
+    }
+
+    @Value("${params.appSecret}")
+    public void setAppSecret(String appSecret) {
+        ApiParams.appSecret = appSecret;
+    }
+}
